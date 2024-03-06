e9e0020378ec90581daf01a48a6309d24eac5484
Dalia <daliaeissa@aucegypt.edu>
2024-03-06 12:35:12 +0200

    welcome window done

--- CinemaProject/CinemaProject/loginwindow.cpp
+++ CinemaProject/CinemaProject/loginwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_loginwindow.h"
 #include "welcomewindow.h"
 #include "registrationwindow.h"
+#include "Users.h"
 
 
 LoginWindow::LoginWindow(QWidget *parent)
@@ -9,6 +10,7 @@ LoginWindow::LoginWindow(QWidget *parent)
     , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
+    ui->label_error->setVisible(false);
 }
 
 LoginWindow::~LoginWindow()
@@ -18,9 +20,21 @@ LoginWindow::~LoginWindow()
 
 void LoginWindow::on_pushButton_login_clicked()
 {
-    hide(); //hide current window
-    WelcomeWindow* welcomewindow = new WelcomeWindow(this); //create the new window
-    welcomewindow->show();
+    for (int i = 0; i<100; i++)
+    {
+        if ((usernames[i] == ui->lineEdit_username->text()) && (passwords[i] == ui->lineEdit_2_password->text()))
+        {
+            hide(); //hide current window
+            WelcomeWindow* welcomewindow = new WelcomeWindow(usernames[i], ages[i], this); //create the new window
+            welcomewindow->show();
+            ui->label_error->setVisible(false);
+        }
+        else
+        {
+            ui->label_error->setVisible(true);
+            //ui->label_error->setText("Error! Wrong username or password!");
+        }
+    }
 }
 
 
--- CinemaProject/CinemaProject/welcomewindow.cpp
+++ CinemaProject/CinemaProject/welcomewindow.cpp
@@ -1,17 +1,34 @@
 #include "welcomewindow.h"
 #include "ui_welcomewindow.h"
 #include <QPixmap>
+#include <QString>
+#include "loginwindow.h"
 
-WelcomeWindow::WelcomeWindow(QWidget *parent)
+WelcomeWindow::WelcomeWindow(QString username, int age, QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::WelcomeWindow)
 {
     ui->setupUi(this);
+    // QString username; //add in constructor as parameters
+    // int age;
+    QString str = QString :: number ( age );
+    ui->label_hello->setText("Hello " + username + " " + str);
+
     QPixmap pix("C:/Users/dalia/Desktop/Spring 2024 semester/CS2 lab/Assignments/Assignment QT/welcomephoto.jpg");
-    ui->label_photowelcome->setPixmap(pix.scaled(540, 360));
+    int w = ui->label_photowelcome->width();
+    int h= ui->label_photowelcome->height();
+    ui->label_photowelcome->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
 }
 
 WelcomeWindow::~WelcomeWindow()
 {
     delete ui;
 }
+
+void WelcomeWindow::on_pushButton_logout_clicked()
+{
+    hide();
+    LoginWindow* loginwindow = new LoginWindow;
+    loginwindow->show();
+}
+
